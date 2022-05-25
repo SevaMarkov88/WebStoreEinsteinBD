290d4cb81eb62a6a507bfd4f698a6e61b4490e3f
Seva Markov <vsevolodmarkoff@yandex.ru>
2022-05-25 13:04:12 +0300

    login added

--- form/form.js
+++ form/form.js
@@ -1,6 +1,9 @@
+import { users } from "./users.js";
 export default class Form {
   constructor(formBlock) {
     this._formBlock = formBlock;
+    this._inputList = this._formBlock.querySelectorAll(".form__input");
+    this._submitBtn = this._formBlock.querySelector(".form__button");
   }
 
   openForm() {
@@ -13,6 +16,14 @@ export default class Form {
     this._removeListener();
   }
 
+  _getInputValuse() {
+    const loginValues = {};
+    this._inputList.forEach((input) => {
+      loginValues[input.name] = input.value;
+    });
+    return loginValues;
+  }
+
   _addListener() {
     document.addEventListener("keydown", (evt) => this._handleEscClose(evt));
     this._formBlock.addEventListener("click", (evt) => {
@@ -23,6 +34,15 @@ export default class Form {
         this._closeForm();
       }
     });
+    this._submitBtn.addEventListener("click", (evt) => {
+      this._handelSubmitForm(evt);
+    });
+  }
+
+  _loginCheck(user) {
+    users.forEach((item) => {
+        return JSON.stringify(item) === JSON.stringify(user);
+    })
   }
 
   _removeListener() {
@@ -35,9 +55,16 @@ export default class Form {
         this._closeForm();
       }
     });
+    this._submitBtn.removeEventListener("click", (evt) => {
+      this._handelSubmitForm(evt);
+    });
   }
 
-  submitForm() {}
+  _handelSubmitForm(evt) {
+    evt.preventDefault();
+    const userInfo = this._getInputValuse();
+    this._loginCheck(userInfo);
+  }
 
   _handleEscClose(evt) {
     const key = evt.key;
---
+++ form/users.js
@@ -0,0 +1,1 @@
+export let users = [{ email: "test@mail.com", password: "12345" }];
